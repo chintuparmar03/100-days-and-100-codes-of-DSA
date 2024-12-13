839a3f37531457801b2cf7acc62d5e127ceda1d0
chintu parmar <chintuparmar603@gmail.com>
2024-12-13 16:08:24 +0530

    day 9 of coding for 100 days

---
+++ palindrom_hilltop.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s;
+        cin >> s;
+        long int n = s.length();
+
+        if (n == 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        bool modified = false;
+
+        for (int i = 0; i < n / 2; ++i) {
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                modified = true;
+                break;
+            }
+        }
+
+        if (!modified) {
+            s[n - 1] = 'b';
+        }
+
+        cout << s << endl;
+    }
+
+    return 0;
+}#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s;
+        cin >> s;
+        long int n = s.length();
+
+        if (n == 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        bool modified = false;
+
+        for (int i = 0; i < n / 2; ++i) {
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                modified = true;
+                break;
+            }
+        }
+
+        if (!modified) {
+            s[n - 1] = 'b';
+        }
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
