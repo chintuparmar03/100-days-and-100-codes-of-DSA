4a965139614bcbed16eb306d17d2aad5c9e8d7bc
chintu parmar <chintuparmar603@gmail.com>
2024-12-28 23:01:35 +0530

    change in code more optimized

--- desert_sroll.cpp
+++ desert_sroll.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool isMagicalScroll(const vector<vector<int>>& matrix, int m, int n) {
-    // Check if the matrix is square
-    if (m != n) return false;
-
-    // Check the conditions for diagonal and non-diagonal elements
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i == j) {
-                // Diagonal element must be non-zero
-                if (matrix[i][j] == 0) return false;
-            } else {
-                // Non-diagonal element must be zero
-                if (matrix[i][j] != 0) return false;
+bool isDiagonal(int matrix[100][100], int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((i == j || i + j == m - 1) && matrix[i][j] == 0) {
+                return false;
+            }
+            if ((i != j && i + j != m - 1) && matrix[i][j] != 0) {
+                return false;
             }
         }
     }
@@ -22,30 +16,22 @@ bool isMagicalScroll(const vector<vector<int>>& matrix, int m, int n) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T;
-    cin >> T;
-
-    while (T--) {
+    int t;
+    cin >> t;
+    while (t--) {
         int m, n;
         cin >> m >> n;
-        
-        vector<vector<int>> matrix(m, vector<int>(n));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        int matrix[100][100];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 cin >> matrix[i][j];
             }
         }
-
-        if (isMagicalScroll(matrix, m, n)) {
-            cout << "true\n";
+        if (isDiagonal(matrix, m, n)) {
+            cout << "true" << endl;
         } else {
-            cout << "false\n";
+            cout << "false" << endl;
         }
     }
-
     return 0;
 }
-
